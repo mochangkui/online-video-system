f99ba35c23af4fc846827858b7c69adce0084c01
mochangkui <825305706@qq.com>
2024-11-10 20:38:26 +0800

    spring初始项目

---
+++ src/main/java/com/course/onlinevideosystem/OnlineVideoSystemApplication.java
@@ -0,0 +1,13 @@
+package com.course.onlinevideosystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineVideoSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineVideoSystemApplication.class, args);
+    }
+
+}
