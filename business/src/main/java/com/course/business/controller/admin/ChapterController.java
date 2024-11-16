8edd0a09caeff99025aa6e5ae07cbae31adac44f
mochangkui <825305706@qq.com>
2024-11-16 23:26:04 +0800

    1、增加ControllerAdvice统一异常处理。2、增加参数校验。3、增加日志AOP，统一日志输出。4、logback增加打印日志跟踪号。

--- business/src/main/java/com/course/business/controller/admin/ChapterController.java
+++ business/src/main/java/com/course/business/controller/admin/ChapterController.java
@@ -1,23 +1,70 @@
 package com.course.business.controller.admin;
 
-import com.course.server.domain.Chapter;
 import com.course.server.dto.ChapterDto;
+import com.course.server.dto.PageDto;
+import com.course.server.dto.ResponseDto;
 import com.course.server.service.ChapterService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.course.server.util.ValidatorUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
-import java.util.List;
 
 @RestController
 @RequestMapping("/admin")
 public class ChapterController {
 
+    private static final Logger LOG = LoggerFactory.getLogger(ChapterController.class);
+    private static final String BUSINESS_NAME = "大章";
+
     @Resource
     private ChapterService chapterService;
 
+    /**
+     * 查询列表
+     * @param pageDto 分页数据
+     * @return 数据
+     */
     @RequestMapping("/list")
-    public List<ChapterDto> list() {
-        return chapterService.list();
+    public ResponseDto<PageDto<ChapterDto>> list(@RequestBody PageDto<ChapterDto> pageDto) {
+        LOG.info("pageDto: {}", pageDto);
+        ResponseDto<PageDto<ChapterDto>> responseDto = new ResponseDto<>();
+        chapterService.list(pageDto);
+        responseDto.setContent(pageDto);
+        return responseDto;
+    }
+
+    /**
+     * 新增大章数据
+     * @param chapterDto 大章数据
+     * @return 大章数据
+     */
+    @RequestMapping("/save")
+    public ResponseDto<ChapterDto> list(@RequestBody ChapterDto chapterDto) {
+        LOG.info("chapterDto: {}", chapterDto);
+
+        // 保存校验
+        ValidatorUtil.require(chapterDto.getName(), "名称");
+        ValidatorUtil.require(chapterDto.getCourseId(), "课程ID");
+        ValidatorUtil.length(chapterDto.getCourseId(), "课程ID", 1, 8);
+
+        ResponseDto<ChapterDto> responseDto = new ResponseDto<>();
+        chapterService.save(chapterDto);
+        responseDto.setContent(chapterDto);
+        return responseDto;
+    }
+
+    /**
+     * 删除大章
+     * @param id 大章id
+     * @return ResponseDto
+     */
+    @DeleteMapping("/delete{id}")
+    public ResponseDto<Object> delete(@PathVariable String id) {
+        LOG.info("id: {}", id);
+        ResponseDto<Object> responseDto = new ResponseDto<>();
+        chapterService.delete(id);
+        return responseDto;
     }
 }
--- server/src/main/java/com/course/server/service/ChapterService.java
+++ server/src/main/java/com/course/server/service/ChapterService.java
@@ -2,10 +2,14 @@ package com.course.server.service;
 
 import com.course.server.domain.Chapter;
 import com.course.server.dto.ChapterDto;
+import com.course.server.dto.PageDto;
 import com.course.server.mapper.ChapterMapper;
+import com.course.server.util.UuidUtil;
 import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
 import javax.annotation.Resource;
 import java.util.ArrayList;
@@ -17,15 +21,54 @@ public class ChapterService {
     @Resource
     private ChapterMapper chapterMapper;
 
-    public List<ChapterDto> list() {
-        PageHelper.startPage(1, 1);
+    /**
+     * 查询大章列表
+     * @param pageDto 分页数据
+     */
+    public void list(PageDto<ChapterDto> pageDto) {
+        PageHelper.startPage(pageDto.getPage(), pageDto.getSize());
         List<Chapter> chapterList = chapterMapper.selectByExample(null);
+        PageInfo<Chapter> pageInfo = new PageInfo<>(chapterList);
+        pageDto.setTotal(pageInfo.getTotal());
         List<ChapterDto> chapterDtoList = new ArrayList<>();
         for (Chapter chapter : chapterList) {
             ChapterDto chapterDto = new ChapterDto();
             BeanUtils.copyProperties(chapter, chapterDto);
             chapterDtoList.add(chapterDto);
         }
-        return chapterDtoList;
+        pageDto.setList(chapterDtoList);
+    }
+
+    /**
+     * 新增大章
+     * @param chapterDto 大章数据
+     */
+    public void save(ChapterDto chapterDto) {
+        Chapter chapter = new Chapter();
+        BeanUtils.copyProperties(chapterDto, chapter);
+        if (StringUtils.isEmpty(chapterDto.getId())) {
+            this.insert(chapter);
+        } else {
+            this.update(chapter);
+        }
+    }
+
+    private void insert(Chapter chapter) {
+        chapter.setId(UuidUtil.getShortUuid());
+        chapterMapper.insert(chapter);
+    }
+
+    private void update(Chapter chapter) {
+        chapterMapper.updateByPrimaryKey(chapter);
+    }
+
+    /**
+     * 删除大章
+     * @param id 大章id
+     */
+    public void delete(String id) {
+        if (StringUtils.isEmpty(id)) {
+            chapterMapper.deleteByPrimaryKey(id);
+        }
     }
 }
