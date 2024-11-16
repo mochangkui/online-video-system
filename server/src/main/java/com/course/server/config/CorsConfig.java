71908d4400ad0733a55b707c9e0c1ee826428ff9
mochangkui <825305706@qq.com>
2024-11-16 14:43:43 +0800

    使用lb://+注册中心名称作路由转发

--- server/src/main/java/com/course/server/config/CorsConfig.java
+++ server/src/main/java/com/course/server/config/CorsConfig.java
@@ -5,17 +5,18 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
-public class CorsConfig implements WebMvcConfigurer {
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedHeaders(CorsConfiguration.ALL)
-                .allowedMethods(CorsConfiguration.ALL)
-                .allowCredentials(true)
-                .maxAge(3600); // 1小时内不需要再预检（发OPTIONS请求）
-    }
-
-}
+// 已在gateway配置，此处注释掉
+//@Configuration
+//public class CorsConfig implements WebMvcConfigurer {
+//
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")
+//                .allowedOrigins("*")
+//                .allowedHeaders(CorsConfiguration.ALL)
+//                .allowedMethods(CorsConfiguration.ALL)
+//                .allowCredentials(true)
+//                .maxAge(3600); // 1小时内不需要再预检（发OPTIONS请求）
+//    }
+//
+//}
